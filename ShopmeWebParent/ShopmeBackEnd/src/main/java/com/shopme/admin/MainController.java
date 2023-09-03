479c01a1828d0a1d47d6cda697a68eaa9966a51c
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-09-03 08:21:35 +0200

    Shopme BackEnd : Using jQuery and Bootstrap

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+	// Http Request : "http://localhost:8080/ShopmeAdmin"
 	@GetMapping("")
 	public String viewHomePage() {
 		return "index";
